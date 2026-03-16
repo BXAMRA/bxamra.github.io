a6659c8f7b0e0fe798ce256c77e2a0616b2ce371
BXAMRA <jassbhamra@outlook.com>
2026-03-17 01:27:02 +0530

    update

--- docs-src/.vitepress/theme/index.ts
+++ docs-src/.vitepress/theme/index.ts
@@ -1,13 +1,8 @@
 // docs/.vitepress/theme/index.ts
-import type { Theme } from "vitepress";
-import DefaultTheme from "vitepress/theme";
-import MyLayout from "./MyLayout.vue";
 
+import DefaultTheme from "vitepress/theme";
 import "./edo.css";
 import "./fiver-social-tweek.css";
 import "./custom.css";
 
-export default {
-  extends: DefaultTheme,
-  Layout: MyLayout,
-} satisfies Theme;
+export default DefaultTheme;
